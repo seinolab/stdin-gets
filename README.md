089ea0b29c6069fef7d3a1f6e41a9329bf18b270
清野 貴博 <seinolab>
2014-11-16 10:30:37 +0900

    0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 stdin-gets
 ==========
+
+Test code
+
+```JavaScript
+var stdin = require("stdin-gets");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+stdin.gets(function (buf, num) {
+  console.log("%d: %s", num, buf)
+
+}, function () {
+  console.log("[EOF]");
+
+});
+```
+
+Result: 
+
+```
+0: var stdin = require("stdin-gets");
+1: 
+2: process.stdin.resume();
+3: process.stdin.setEncoding("utf8");
+4: 
+5: stdin.gets(function (buf, num) {
+6:   console.log("%d: %s", num, buf)
+7: 
+8: }, function () {
+9:   console.log("[EOF]");
+10: 
+11: });
+12: 
+[EOF]
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+var stdin = exports;
+
+/**
+ * 行単位に処理するための標準入力管理用関数
+ * @param {Function} callback_for_each_line 行ごとに呼ばれるコールバック関数
+ * @param {Function} [callback_for_eof] EOF に到達したときに呼ばれるコールバック関数
+ */
+exports.gets = function (callback_for_each_line, callback_for_eof) {
+
+  var fragment = "";
+  var linenum = 0;
+
+  process.stdin.on("data", function (chunk) {
+
+    if (chunk === "") {
+      return;
+    }
+
+    var lines = chunk.split("\n");
+    lines[0] = fragment + lines[0];
+    fragment = lines.pop();
+
+    callback_for_each_line && lines.forEach(function (str) {
+
+      callback_for_each_line(str, linenum);
+      linenum = linenum + 1;
+
+    });
+  });
+
+  process.stdin.on("end", function () {
+
+    callback_for_each_line && callback_for_each_line(fragment, linenum);
+    callback_for_eof && callback_for_eof();
+
+  });
+
+};
+
+/**
+ * CSV ファイルを読み込んで処理するための標準入力管理用関数
+ * @param {Array} interpreters CSV の各列のデータを解釈するための関数が入った配列
+ * @param {Function} callback_for_each_line 行ごとに呼ばれるコールバック関数
+ * @param {Function} [callback_for_eof] EOF に到達したときに呼ばれるコールバック関数
+ */
+exports.csv = function (interpreters, callback_for_each_line, callback_for_eof) {
+
+  stdin.gets(function (buf, linenum) {
+
+    var csv = buf.split(",").map(function (e, i) {
+      return typeof interpreters[i] === "function" ? interpreters[i](e) : e;
+    });;
+
+    callback_for_each_line(csv, linenum);
+
+  }, callback_for_eof);
+
+};
